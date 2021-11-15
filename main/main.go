1d21bf6b7b03a95a368c7522824b667b20125a70
southli <961065551@qq.com>
2021-11-15 18:30:09 +0800

    验证代码的实例，在该实例中定义结构体Foo和方法Sum，然后将这两个注册到Server中，并启动RPC服务，即开始服务。 之后在main方法中对这个服务发起RPC请求，并打印对应的结果。

--- main/main.go
+++ main/main.go
@@ -1,16 +1,28 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"log"
 	"myRpc"
-	"myRpc/codec"
 	"net"
+	"sync"
 	"time"
 )
 
+type Foo int
+type Args struct {
+	Num1, Num2 int
+}
+
+func (f Foo) Sum(args Args, reply *int) error {
+	*reply = args.Num1 + args.Num2
+	return nil
+}
+
 func startServer(addr chan string) {
+	var foo Foo
+	if err := myRpc.Register(&foo); err != nil {
+		log.Fatal("register error:", err)
+	}
 	l, err := net.Listen("tcp", ":0")
 	if err != nil {
 		log.Fatal("network error:", err)
@@ -21,23 +33,26 @@ func startServer(addr chan string) {
 }
 
 func main() {
+	log.SetFlags(0)
 	addr := make(chan string)
 	go startServer(addr)
+	client, _ := myRpc.Dial("tcp", <-addr)
+	defer func() { _ = client.Close() }()
 
-	conn, _ := net.Dial("tcp", <-addr)
-	defer func() { _ = conn.Close() }()
 	time.Sleep(time.Second)
-	_ = json.NewEncoder(conn).Encode(myRpc.DefaultOption)
-	cc := codec.NewGobCodec(conn)
+	// send request & receive response
+	var wg sync.WaitGroup
 	for i := 0; i < 5; i++ {
-		h := &codec.Header{
-			ServiceMethod: "foo.Sum",
-			Seq:           uint64(i),
-		}
-		_ = cc.Write(h, fmt.Sprintf("myRpc req %d", h.Seq))
-		_ = cc.ReadHeader(h)
-		var reply string
-		_ = cc.ReadBody(&reply)
-		log.Println("reply:", reply)
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			args := &Args{Num1: i, Num2: i * i}
+			var reply int
+			if err := client.Call("Foo.Sum", args, &reply); err != nil {
+				log.Fatal("call Foo.Sum error:", err)
+			}
+			log.Printf("%d + %d = %d", args.Num1, args.Num2, reply)
+		}(i)
 	}
+	wg.Wait()
 }
