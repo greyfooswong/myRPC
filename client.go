80ae221438cb2068aae02be89c64890cff8453e4
southli <961065551@qq.com>
2021-11-17 16:32:40 +0800

    浏览器通过代理服务器发起HTTPS请求时，由于请求的站点地址和端口号都是加密保存在HTTPS请求报文头中的，代理服务器如何知道往哪儿发送请求呢？ 为了解决这个问题，浏览器通过HTTP明文形式向代理服务器发送一个CONNECT请求告诉代理服务器目标地址和端口，代理服务器接收到这个请求后，会在对应端口与目标站点及案例一个TCP连接，连接及案例成功后返回HTTP200状态码告诉浏览器与该站点的加密通道已经完成。接下来代理服务器仅需传输浏览器和服务器之间的加密数据包即可，代理服务器无需解析HTTPS报文。 这个过程其实是通过代理服务器将HTTP协议转换为hTTPS协议的过程。对RPC服务器来说，需要做的是将HTTP协议转换为RPC协议，对客户端来说，需要新增HTTP CONNECT请求创建连接的逻辑。
    客户端要做的，就是发起CONNECT请求，检查返回状态码即可成功建立连接。
    通过HTTP CONNECT请求建立连接之后，后续的通信过程就交给NewCLient了。

--- client.go
+++ client.go
@@ -5,6 +5,7 @@
 package myRpc
 
 import (
+	"bufio"
 	"context"
 	"encoding/json"
 	"errors"
@@ -13,50 +14,11 @@ import (
 	"log"
 	"myRpc/codec"
 	"net"
+	"net/http"
+	"strings"
 	"sync"
 	"time"
 )
-
-type clientResult struct {
-	client *Client
-	err    error
-}
-type newClientFunc func(conn net.Conn, opt *Option) (client *Client, err error)
-
-func dialTimeout(f newClientFunc, network, address string, opts ...*Option) (client *Client, err error) {
-	opt, err := parseOptions(opts...)
-	if err != nil {
-		return nil, err
-	}
-	conn, err := net.DialTimeout(network, address, opt.ConnectTimeout)
-	if err != nil {
-		return nil, err
-	}
-	defer func() {
-		if err != nil {
-			_ = conn.Close()
-		}
-	}()
-	ch := make(chan clientResult)
-	go func() {
-		client, err := f(conn, opt)
-		ch <- clientResult{client: client, err: err}
-	}()
-	if opt.ConnectTimeout == 0 {
-		result := <-ch
-		return result.client, result.err
-	}
-	select {
-	case <-time.After(opt.ConnectTimeout):
-		return nil, fmt.Errorf("rpc client connect timeout : expect within %s", opt.ConnectTimeout)
-	case result := <-ch:
-		return result.client, result.err
-	}
-}
-
-func Dial(network, address string, opts ...*Option) (*Client, error) {
-	return dialTimeout(NewClient, network, address, opts...)
-}
 
 // Call represents an active RPC.
 type Call struct {
@@ -226,11 +188,10 @@ func (client *Client) Call(ctx context.Context, serviceMethod string, args, repl
 	select {
 	case <-ctx.Done():
 		client.removeCall(call.Seq)
-		return errors.New("rpc client : call failed:" + ctx.Err().Error())
+		return errors.New("rpc client: call failed: " + ctx.Err().Error())
 	case call := <-call.Done:
 		return call.Error
 	}
-	return call.Error
 }
 
 func parseOptions(opts ...*Option) (*Option, error) {
@@ -274,4 +235,89 @@ func newClientCodec(cc codec.Codec, opt *Option) *Client {
 	}
 	go client.receive()
 	return client
+}
+
+type clientResult struct {
+	client *Client
+	err    error
+}
+
+type newClientFunc func(conn net.Conn, opt *Option) (client *Client, err error)
+
+func dialTimeout(f newClientFunc, network, address string, opts ...*Option) (client *Client, err error) {
+	opt, err := parseOptions(opts...)
+	if err != nil {
+		return nil, err
+	}
+	conn, err := net.DialTimeout(network, address, opt.ConnectTimeout)
+	if err != nil {
+		return nil, err
+	}
+	// close the connection if client is nil
+	defer func() {
+		if err != nil {
+			_ = conn.Close()
+		}
+	}()
+	ch := make(chan clientResult)
+	go func() {
+		client, err := f(conn, opt)
+		ch <- clientResult{client: client, err: err}
+	}()
+	if opt.ConnectTimeout == 0 {
+		result := <-ch
+		return result.client, result.err
+	}
+	select {
+	case <-time.After(opt.ConnectTimeout):
+		return nil, fmt.Errorf("rpc client: connect timeout: expect within %s", opt.ConnectTimeout)
+	case result := <-ch:
+		return result.client, result.err
+	}
+}
+
+// Dial connects to an RPC server at the specified network address
+func Dial(network, address string, opts ...*Option) (*Client, error) {
+	return dialTimeout(NewClient, network, address, opts...)
+}
+
+// NewHTTPClient new a Client instance via HTTP as transport protocol
+func NewHTTPClient(conn net.Conn, opt *Option) (*Client, error) {
+	_, _ = io.WriteString(conn, fmt.Sprintf("CONNECT %s HTTP/1.0\n\n", defaultRPCPath))
+
+	// Require successful HTTP response
+	// before switching to RPC protocol.
+	resp, err := http.ReadResponse(bufio.NewReader(conn), &http.Request{Method: "CONNECT"})
+	if err == nil && resp.Status == connected {
+		return NewClient(conn, opt)
+	}
+	if err == nil {
+		err = errors.New("unexpected HTTP response: " + resp.Status)
+	}
+	return nil, err
+}
+
+// DialHTTP connects to an HTTP RPC server at the specified network address
+// listening on the default HTTP RPC path.
+func DialHTTP(network, address string, opts ...*Option) (*Client, error) {
+	return dialTimeout(NewHTTPClient, network, address, opts...)
+}
+
+// XDial calls different functions to connect to a RPC server
+// according the first parameter rpcAddr.
+// rpcAddr is a general format (protocol@addr) to represent a rpc server
+// eg, http@10.0.0.1:7001, tcp@10.0.0.1:9999, unix@/tmp/geerpc.sock
+func XDial(rpcAddr string, opts ...*Option) (*Client, error) {
+	parts := strings.Split(rpcAddr, "@")
+	if len(parts) != 2 {
+		return nil, fmt.Errorf("rpc client err: wrong format '%s', expect protocol@addr", rpcAddr)
+	}
+	protocol, addr := parts[0], parts[1]
+	switch protocol {
+	case "http":
+		return DialHTTP("tcp", addr, opts...)
+	default:
+		// tcp, unix or other transport protocol
+		return Dial(protocol, addr, opts...)
+	}
 }
